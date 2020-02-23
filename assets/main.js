dd0ea5dbf789986e77414cf266926812ec216518
David Nelson <nelsondavidmark@gmail.com>
2020-02-22 19:03:00 -0500

    Fix main.js file to have to correct image name.

--- assets/main.js
+++ assets/main.js
@@ -77,73 +77,73 @@ window.onscroll = function() {
 const imgData = {
   airKoalaty: [
     {
-      src: "./images/projects/air-koalaty-report/air-p1.jpg",
+      src: "./images/projects/air-koalaty-report/air-p1-compressed.jpg",
       alt: "Air KOALAty Report landing page"
     },
     {
-      src: "./images/projects/air-koalaty-report/air-p2.jpg",
+      src: "./images/projects/air-koalaty-report/air-p2-compressed.jpg",
       alt: "Air KOALAty Report google autocomplete places"
     },
     {
-      src: "./images/projects/air-koalaty-report/air-p3.jpg",
+      src: "./images/projects/air-koalaty-report/air-p3-compressed.jpg",
       alt: "Air KOALAty Report login with AuthO"
     },
     {
-      src: "./images/projects/air-koalaty-report/air-p4.jpg",
+      src: "./images/projects/air-koalaty-report/air-p4-compressed.jpg",
       alt: "Air KOALAty Report list page"
     }
   ],
   pokeSurvey: [
     {
-      src: "./images/projects/poke-survey/poke-survey-p1.jpg",
+      src: "./images/projects/poke-survey/poke-survey-p1-compressed.jpg",
       alt: "Poke-Survey landing page"
     },
     {
-      src: "./images/projects/poke-survey/poke-survey-p2.jpg",
+      src: "./images/projects/poke-survey/poke-survey-p2-compressed.jpg",
       alt: "Poke-Survey vote page"
     },
     {
-      src: "./images/projects/poke-survey/poke-survey-p3.jpg",
+      src: "./images/projects/poke-survey/poke-survey-p3-compressed.jpg",
       alt: "Poke-Survey results page"
     },
     {
-      src: "./images/projects/poke-survey/poke-survey-p4.jpg",
+      src: "./images/projects/poke-survey/poke-survey-p4-compressed.jpg",
       alt: "Poke-Survey contact page"
     }
   ],
   portfolioV1: [
     {
-      src: "./images/projects/portfolio-v1/portfolio-p1.jpg",
+      src: "./images/projects/portfolio-v1/portfolio-p1-compressed.jpg",
       alt: "Portfolio v1 landing page"
     },
     {
-      src: "./images/projects/portfolio-v1/portfolio-p2.jpg",
+      src: "./images/projects/portfolio-v1/portfolio-p2-compressed.jpg",
       alt: "Portfolio v1 contact form"
     },
     {
-      src: "./images/projects/portfolio-v1/portfolio-p3.jpg",
+      src: "./images/projects/portfolio-v1/portfolio-p3-compressed.jpg",
       alt: "Portfolio v1 unit tests"
     },
     {
-      src: "./images/projects/portfolio-v1/portfolio-p4.jpg",
+      src: "./images/projects/portfolio-v1/portfolio-p4-compressed.jpg",
       alt: "Portfolio v1 cypress tests"
     }
   ],
   shareFamilyRecipes: [
     {
-      src: "./images/projects/share-family-recipes/sfr-p1.jpg",
+      src: "./images/projects/share-family-recipes/sfr-p1-compressed.jpg",
       alt: "Share family recipes landing page with search bar"
     },
     {
-      src: "./images/projects/share-family-recipes/sfr-p2.jpg",
+      src: "./images/projects/share-family-recipes/sfr-p2-compressed.jpg",
       alt: "Share family recipes list of recipes"
     },
     {
-      src: "./images/projects/share-family-recipes/sfr-p3.jpg",
+      src: "./images/projects/share-family-recipes/sfr-p3-compressed.jpg",
       alt: "Share family recipes figma design"
     },
     {
-      src: "./images/projects/share-family-recipes/sfr-p4.jpg",
+      src: "./images/projects/share-family-recipes/sfr-p4-compressed.jpg",
       alt: "Share family recipes unit, end-to-end, and back end tests"
     }
   ]
